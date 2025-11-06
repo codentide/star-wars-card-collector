297c05d3ac40aefaa5d6335f8da6f0473f135a42
Codentide <marcodelboccio77@gmail.com>
2025-11-06 09:38:37 -0500

    Init: Adding font

---
+++ src/components/layout/Header/Header.tsx
@@ -0,0 +1,20 @@
+import { NavLink } from 'react-router'
+import './header.scss'
+
+export const Header = () => {
+  return (
+    <header className="page-header">
+      <h1 className="page-header__title">Jedi Collector</h1>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/" children="Album" />
+          </li>
+          <li>
+            <NavLink to="/unbox" children="Unbox" />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
