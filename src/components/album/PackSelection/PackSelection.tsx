e4f64f9b62fe17d6125c62bef2d11d9f463401cf
Codentide <marcodelboccio77@gmail.com>
2025-11-07 22:16:10 -0500

    UI: Adding new animatin on PackSelection

--- src/components/album/PackSelection/PackSelection.tsx
+++ src/components/album/PackSelection/PackSelection.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useAlbumActions } from '../../../stores/album.store'
 import type { SecretPack as SecretPackType } from '../../../types/album.types'
 
@@ -8,15 +9,23 @@ interface Props {
 }
 
 export const PackSelection = ({ packs }: Props) => {
+  const [isFadingOut, setIsFadingOut] = useState<boolean>(false)
   const { openSecretPack } = useAlbumActions()
+
+  const handleOpen = (index: number) => {
+    setIsFadingOut(true)
+
+    setTimeout(() => openSecretPack(index), 300)
+  }
 
   return (
     <ul className="pack-selection">
       {packs.map((_, index) => (
         <button
           key={index}
-          className="secret-pack"
-          onClick={() => openSecretPack(index)}
+          className={`secret-pack ${isFadingOut ? 'secret-pack--exiting' : ''}`}
+          disabled={isFadingOut}
+          onClick={() => handleOpen(index)}
         />
       ))}
     </ul>
